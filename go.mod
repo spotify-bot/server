75b00c3750e49914234a24933fdaf3380460ff7f
mohammadkarimi23 <eecs.mk74@gmail.com>
2020-12-30 20:16:39 +0100

    Add Telegram bot start

--- go.mod
+++ go.mod
@@ -4,6 +4,8 @@ go 1.13
 
 require (
 	github.com/caarlos0/env/v6 v6.4.0
+	github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible
 	github.com/joho/godotenv v1.3.0
 	github.com/labstack/echo/v4 v4.1.17
+	github.com/technoweenie/multipartstreamer v1.0.1 // indirect
 )
---
+++ internal/platform/telegram/bot.go
@@ -0,0 +1,38 @@
+package telegram
+
+import (
+	"log"
+
+	"github.com/go-telegram-bot-api/telegram-bot-api"
+)
+
+//type telegramServer struct{}
+
+func Start(token string) {
+	bot, err := tgbotapi.NewBotAPI(token) //FIXME change to use configs
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates, err := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message == nil { // ignore any non-Message Updates
+			continue
+		}
+
+		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+		msg.ReplyToMessageID = update.Message.MessageID
+
+		bot.Send(msg)
+	}
+}
