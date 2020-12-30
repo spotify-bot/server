6992b5afe2b2ae3f94d31a9ec4b6bcddab0e8467
Mohammad Hossein Heydari <mdh.heydari@gmail.com>
2020-12-30 20:34:01 +0100

    ✨ Add storage for oauth tokens

--- cmd/webserver/main.go
+++ cmd/webserver/main.go
@@ -1,26 +1,40 @@
 package main
 
 import (
+	"context"
 	"log"
 	"os"
 
 	"github.com/caarlos0/env/v6"
 	"github.com/joho/godotenv"
+	"github.com/koskalak/mamal/internal/mongo"
 	"github.com/koskalak/mamal/internal/webserver"
 )
 
 type config struct {
-	Env       string `env:"ENV" envDefault:"development"`
-	Addr      string `env:"ADDR" envDefault:":8000"`
-	JWTSecret string `env:"JWT_SECRET,required"`
+	Env                 string `env:"ENV" envDefault:"development"`
+	Addr                string `env:"ADDR" envDefault:":8000"`
+	JWTSecret           string `env:"JWT_SECRET,required"`
+	MongoDSN            string `env:"MONGODB_DSN" envDefault:"mongodb://localhost:27017/mamal"`
+	SpotifyClientID     string `env:"SPOTIFY_CLIENT_ID"`
+	SpotifyClientSecret string `env:"SPOTIFY_CLIENT_SECRET"`
 }
 
 func main() {
 	cfg := newConfig()
+	ctx := context.Background()
 
-	w := webserver.New()
+	mongoStorage, err := mongo.NewMongoStorage(ctx, mongo.MongoStorageOptions{
+		DSN: cfg.MongoDSN,
+	})
 
-	err := w.Start(cfg.Addr)
+	w := webserver.New(webserver.WebServerOptions{
+		Mongo:               mongoStorage,
+		SpotifyClientID:     cfg.SpotifyClientID,
+		SpotifyClientSecret: cfg.SpotifyClientSecret,
+	})
+
+	err = w.Start(cfg.Addr)
 
 	if err != nil {
 		log.Fatalln("Cannot start server", err)
